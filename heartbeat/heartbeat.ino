188ffd932a250a194aeed66e6d703fe733c21edb
curti <curti@LAPTOP-G0FAMB2H.dhcp.wustl.edu>
2018-01-17 21:33:12 -0600

    Studio Zero

---
+++ heartbeat/heartbeat.ino
@@ -0,0 +1,20 @@
+int n = 0;
+int ledPin = 13;
+long milli = 0;
+void setup() {
+  Serial.begin(9600);
+  Serial.println("0 Seconds have elapsed");
+  pinMode(ledPin, OUTPUT);
+}
+void loop() {
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(1000);
+  n = n + 1;
+  milli = millis();
+  Serial.println(milli);
+  Serial.print(n);
+  Serial.println(" seconds have elapsed");
+
+}
